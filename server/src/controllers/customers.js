dc51904b8af44e9a26b67173b2473910924799ad
Polar502 <marioarita502@gmail.com>
2022-11-02 00:15:57 -0600

    Rama Mario

--- server/src/controllers/customers.js
+++ server/src/controllers/customers.js
@@ -7,6 +7,15 @@ export const getCustomers = async (req, res) => {
   res.json(result.rows)
 }
 
+export const getCustomer = async (req, res) => {
+  const connection = await oracledb.getConnection(config)
+  const result = await connection.execute(
+    'SELECT * FROM customers WHERE c_id = ?',
+    [req.params.c_id]
+  )
+  res.json(result.rows)
+}
+
 export const getEmployees = async (req, res) => {
   const connection = await oracledb.getConnection(config)
   const result = await connection.execute('SELECT * FROM employees')
