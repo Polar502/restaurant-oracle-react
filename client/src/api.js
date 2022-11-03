60b0ec42a56ef271050188208e37e594275dce97
Nery Lopez <nerya.lopezs.18@gmail.com>
2022-11-03 01:53:47 -0600

    Completando API client

--- client/src/api.js
+++ client/src/api.js
@@ -18,32 +18,28 @@ export const getUsers = async (tipoUser) => {
   return await res.json();
 };
 
-// export const saveTask = async (newTask) => {
-//   const res = await fetch(API, {
-//     method: "POST",
-//     headers: {
-//       Accept: "application/json",
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(newTask),
-//   });
-//   return await res.json();
-// };
+//nuevos
+export const saveUser = async (tipoUser, newUser) => {
+  const res = await fetch(`${API}/${tipoUser}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newUser),
+  });
+  return await res.json();
+};
 
-// export const getTask = async (taskId) => {
-//   const res = await fetch(`${API}/${taskId}`);
-//   return await res.json();
-// };
-
-// export const updateTask = async (taskId, newTask) => {
-//   console.log(taskId, newTask)
-//   const res = await fetch(`${API}/${taskId}`, {
-//     method: "PUT",
-//     headers: {
-//       Accept: "application/json",
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(newTask),
-//   });
-//   return res;
-// };
+export const updateUser = async (tipoUser, id, newUser) => {
+  console.log(id, newUser)
+  const res = await fetch(`${API}/${tipoUser}/${id}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newUser),
+  });
+  return res;
+};
