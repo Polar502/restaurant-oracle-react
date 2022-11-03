4d22929aec6cf0cbc60d0ae0eed9b4443010e36d
Polar502 <marioarita502@gmail.com>
2022-11-03 01:41:26 -0600

    add autoCommit oracledb

--- server/src/controllers/customers.controller.js
+++ server/src/controllers/customers.controller.js
@@ -1,51 +1,77 @@
 import oracledb from 'oracledb'
 import { config } from '../config.js'
 
+oracledb.autoCommit = true
+
+let connection
+
+const closeConnection = async () => {
+  if (connection) {
+    try {
+      await connection.close()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
 export const getCustomers = async (req, res) => {
   try {
-    const connection = await oracledb.getConnection(config)
+    connection = await oracledb.getConnection(config)
     const result = await connection.execute('SELECT * FROM customers')
     res.json(result.rows)
   } catch (error) {
-    console.log(error)
+    console.error(error)
+  } finally {
+    closeConnection()
   }
 }
 
 export const postCustomer = async (req, res) => {
+  const id = req.body.id
+  const name = req.body.name
+  const address = req.body.id
+  const phone = req.body.id
   try {
-    const connection = await oracledb.getConnection(config)
-    const result = await connection.execute(
+    connection = await oracledb.getConnection(config)
+    await connection.execute(
       'INSERT INTO customers (c_id, c_name, c_address, c_phone) VALUES ( :id , :name , :address, :phone )',
-      [req.body.id, req.body.name, req.body.address, req.body.phone]
+      [id, name, address, phone]
     )
-    console.log(result)
     res.sendStatus(204)
   } catch (error) {
-    console.log(error)
+    console.error(error)
+  } finally {
+    closeConnection()
   }
 }
 
 export const putCustomer = async (req, res) => {
+  const body = req.body
+  const id = req.query.id
   try {
-    const connection = await oracledb.getConnection(config)
-    await connection.execute('UPDATE customers SET ? WHERE c_id = :id', [
-      req.body,
-      req.params.id,
+    connection = await oracledb.getConnection(config)
+    await connection.execute('UPDATE customers SET :body WHERE c_id = :id', [
+      body,
+      id,
     ])
     res.sendStatus(204)
   } catch (error) {
-    console.log(error)
+    console.error(error)
+  } finally {
+    closeConnection()
   }
 }
 
 export const deleteCustomer = async (req, res) => {
+  const id = req.params.id
   try {
-    const connection = await oracledb.getConnection(config)
-    await connection.execute('DELETE FROM customers WHERE c_id = :id', [
-      req.params.id,
-    ])
+    connection = await oracledb.getConnection(config)
+    await connection.execute('DELETE FROM customers WHERE c_id = :id', [id])
     res.sendStatus(204)
   } catch (error) {
-    console.log(error)
+    console.error(error)
+  } finally {
+    closeConnection()
   }
 }
--- server/src/controllers/employees.controller.js
+++ server/src/controllers/employees.controller.js
@@ -1,52 +1,81 @@
 import oracledb from 'oracledb'
 import { config } from '../config.js'
 
+oracledb.autoCommit = true
+
+let connection
+
+const closeConnection = async () => {
+  if (connection) {
+    try {
+      await connection.close()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
 export const getEmployees = async (req, res) => {
   try {
-    const connection = await oracledb.getConnection(config)
+    connection = await oracledb.getConnection(config)
     const result = await connection.execute('SELECT * FROM employees')
     res.json(result.rows)
   } catch (error) {
-    console.log(error)
+    console.error(error)
+  } finally {
+    closeConnection()
   }
 }
 
 // Pendiente
 export const postEmployee = async (req, res) => {
+  const id = req.body.id
+  const name = req.body.name
+  const address = req.body.phone
+  const phone = req.body.address
+  const salary = req.body.salary
+  const job = req.body.job
   try {
-    const connection = await oracledb.getConnection(config)
-    const result = await connection.execute(
-      'INSERT INTO employees (c_id, c_name, c_address, c_phone) VALUES ( :id , :name , :address, :phone )',
-      [req.body.id, req.body.name, req.body.address, req.body.phone]
+    connection = await oracledb.getConnection(config)
+    await connection.execute(
+      'INSERT INTO employees (e_id, e_name, e_phone, e_address, e_salary, j_id) VALUES ( :id, :name, :phone, :address, :salary, :job)',
+      [id, name, phone, address, salary, job]
     )
-    console.log(result)
     res.sendStatus(204)
   } catch (error) {
-    console.log(error)
+    console.error(error)
+  } finally {
+    closeConnection()
   }
 }
 
 export const putEmployee = async (req, res) => {
+  const body = req.body
+  const id = req.params.id
+
   try {
-    const connection = await oracledb.getConnection(config)
+    connection = await oracledb.getConnection(config)
     await connection.execute('UPDATE employees SET ? WHERE e_id = :id', [
-      req.body,
-      req.params.id,
+      body,
+      id,
     ])
     res.sendStatus(204)
   } catch (error) {
-    console.log(error)
+    console.error(error)
+  } finally {
+    closeConnection()
   }
 }
 
 export const deleteEmployee = async (req, res) => {
+  const id = req.params.id
   try {
-    const connection = await oracledb.getConnection(config)
-    await connection.execute('DELETE FROM employees WHERE e_id = :id', [
-      req.params.id,
-    ])
+    connection = await oracledb.getConnection(config)
+    await connection.execute('DELETE FROM employees WHERE e_id = :id', [id])
     res.sendStatus(204)
   } catch (error) {
-    console.log(error)
+    console.error(error)
+  } finally {
+    closeConnection()
   }
 }
